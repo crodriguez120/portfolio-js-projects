88bf3f6f5c8312e3bb5e6387d450dc4f8166fb42
Carlos Rodriguez <carlosisaackr@gmail.com>
2024-12-26 14:49:15 -0400

    feat: add logic for winner determination, score tracking, and reset scores.

--- spock-rock-game/script.js
+++ spock-rock-game/script.js
@@ -18,6 +18,8 @@ const computerSpock = document.getElementById('computerSpock');
 
 const allGameIcons = document.querySelectorAll(".far");
 
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
 let computerChoice = '';
 
 const choices = {
@@ -33,6 +35,18 @@ function resetSelected() {
   allGameIcons.forEach(icon => {
     icon.classList.remove('selected');
   });
+}
+
+// Reset Score, & playerChoice/computerChoice 
+function resetAll() {
+  playerScoreNumber = 0;
+  computerScoreNumber = 0;
+  playerChoiceEl.textContent = '';
+  computerChoiceEl.textContent = '';
+  playerScoreEl.textContent = playerScoreNumber;
+  computerScoreEl.textContent = computerScoreNumber;
+  resultTextEl.textContent = '';
+  resetSelected();
 }
 
 // Random Computer Choice
@@ -57,39 +71,58 @@ function displayComputerChoice() {
   switch (computerChoice) {
     case 'rock':
       computerRock.classList.add('selected');
-      computerChoiceEl.textContent = '--- Rock';
       break;
     case 'paper':
       computerPaper.classList.add('selected');
-      computerChoiceEl.textContent = '--- Paper';
+      computerChoiceEl.textContent = ' --- Paper';
       break;
     case 'scissors':
       computerScissors.classList.add('selected');
-      computerChoiceEl.textContent = '--- Scissors';
+      computerChoiceEl.textContent = ' --- Scissors';
       break;
     case 'lizard':
       computerLizard.classList.add('selected');
-      computerChoiceEl.textContent = '--- Lizard';
+      computerChoiceEl.textContent = ' --- Lizard';
       break;
     case 'spock':
       computerSpock.classList.add('selected');
-      computerChoiceEl.textContent = '--- Spock';
+      computerChoiceEl.textContent = ' --- Spock';
       break;
     default:
       break;
   }
 }
 
+// Check result, increase scores, update resultText
+function updateScore(playerChoice) {
+  if (playerChoice === computerChoice) {
+    resultTextEl.textContent = "It's a tie.";
+  } else {
+    const choice = choices[playerChoice];
+    
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+      resultTextEl.textContent = "You Won!";
+      playerScoreNumber++;
+      playerScoreEl.textContent = playerScoreNumber;
+    } else {
+      resultTextEl.textContent = "You Lost!";
+      computerScoreNumber++;
+      computerScoreEl.textContent = computerScoreNumber;
+    }
+  }
+}
+
 // Call function to process turn
-function checkResults() {
+function checkResults(playerChoice) {
   resetSelected();
   computerRandomChoice();
   displayComputerChoice();
+  updateScore(playerChoice);
 }
 
 // Passing player selection value and styling icons
 function select(playerChoice) {
-  checkResults()
+  checkResults(playerChoice)
   // Add 'selected' styling and playerChoice
   switch (playerChoice) {
     case 'rock':
@@ -98,22 +131,24 @@ function select(playerChoice) {
       break;
     case 'paper':
       playerPaper.classList.add('selected');
-      playerChoiceEl.textContent = '--- Paper';
+      playerChoiceEl.textContent = ' --- Paper';
       break;
     case 'scissors':
       playerScissors.classList.add('selected');
-      playerChoiceEl.textContent = '--- Scissors';
+      playerChoiceEl.textContent = ' --- Scissors';
       break;
     case 'lizard':
       playerLizard.classList.add('selected');
-      playerChoiceEl.textContent = '--- Lizard';
+      playerChoiceEl.textContent = ' --- Lizard';
       break;
     case 'spock':
       playerSpock.classList.add('selected');
-      playerChoiceEl.textContent = '--- Spock';
+      playerChoiceEl.textContent = ' --- Spock';
       break;
     default:
       break;
   }
 }
 
+// On startup, set initial values
+resetAll();
